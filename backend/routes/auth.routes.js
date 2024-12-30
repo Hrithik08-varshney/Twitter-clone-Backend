f2bf3f66893f4b4d43b5f774606101c0b4f286ac
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-12-30 23:01:34 +0530

    auth routes file updated

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,16 +1,15 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 //The router object allows you to define routes separately and organize them logically.
 // Later, this router will be linked to the main app using app.use().
 const router = express.Router();
 
 //The path for this route. When a request is made to /signup, this route will handle it.
-router.get("/signup", (req, res) => {
+router.post("/signup", signup);
 
-  //Sends a JSON response to the client. Here, it sends a JSON object with a data property.
-  res.json({
-    data: "You hit the signup endpoint",
-  });
-});
+router.post("/login",login);
+
+router.post("/logout", logout);
 
 export default router;
