01514e9ee7f4b3c4c92835686f9633ce2abf220b
Hrithik Varshney <hritik.varshney2000@gmail.com>
2024-12-31 23:29:45 +0530

    auth controller changes added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,20 +1,66 @@
+import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 export const signup = async (req, res) => {
-  //Sends a JSON response to the client. Here, it sends a JSON object with a data property.
-  res.json({
-    data: "You hit the signup endpoint",
-  });
+  try {
+    const { fullName, username, email, password } = req.body;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    //hashpassword
+    const salt = await bcrypt.genSalt(10); //Generates a salt with 10 rounds.
+    const hashedPassword = await bcrypt.hash(password, salt); //Hashes the password with the salt.
+
+    //Creates a new User document using the User model and the hashed password.
+    const newUser = new User({
+      fullName,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+        //A function that generates an authentication token (likely a JWT) and sets it in a cookie.
+      generateTokenAndSetCookie(newUser._id, res);
+      await newUser.save();
+      res.status(200).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+        fullName: newUser.fullName,
+        followersd: newUser.followers,
+        following: newUser.following,
+        profileImg: newUser.profileImg,
+        coverImg: newUser.coverImg,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = async (req, res) => {
-    //Sends a JSON response to the client. Here, it sends a JSON object with a data property.
-    res.json({
-      data: "You hit the login endpoint",
-    });
-  };
+  res.json({
+    data: "You hit the login endpoint",
+  });
+};
 
-  export const logout = async (req, res) => {
-    //Sends a JSON response to the client. Here, it sends a JSON object with a data property.
-    res.json({
-      data: "You hit the logout endpoint",
-    });
-  };
+export const logout = async (req, res) => {
+  res.json({
+    data: "You hit the logout endpoint",
+  });
+};
