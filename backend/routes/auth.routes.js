9601ae74d4dce235783d13c01fac8a0e2d43883d
Hrithik Varshney <hritik.varshney2000@gmail.com>
2025-01-13 00:14:05 +0530

    documentation added for authroutes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,15 +1,26 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import {
+  getMe,
+  login,
+  logout,
+  signup,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 //The router object allows you to define routes separately and organize them logically.
 // Later, this router will be linked to the main app using app.use().
 const router = express.Router();
 
+//get current user information with protectRoute as middleware
+router.get("/me", protectRoute, getMe);
+
 //The path for this route. When a request is made to /signup, this route will handle it.
 router.post("/signup", signup);
 
-router.post("/login",login);
+//login
+router.post("/login", login);
 
+//logout
 router.post("/logout", logout);
 
 export default router;
