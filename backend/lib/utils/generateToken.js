aa7d8e7f5cdc57508cb6169fd3587b2cd2fac440
Hrithik Varshney <hritik.varshney2000@gmail.com>
2025-01-15 23:17:38 +0530

    generate token documentation

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -8,7 +8,7 @@ export const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, //milliseconds
     httpOnly: true, //prevent XSS attacks cross-site scripting attacks
     samesite: "strict", //CSRF attacks cross-site request forgery attack
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== "development", //check to determine whether the environment is deployement or not
   });
 };
 
